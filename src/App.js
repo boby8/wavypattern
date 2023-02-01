50801e31d9f8d2156e08a2ab346f7da8a9e6d341
boby8 <singhboby84633@gmail.com>
2023-02-01 12:37:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Wave from '../src/component/wava';
+
+function App() {
+  return (
+    <>
+    <Wave/>  
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/wava.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './wave.css';
+
+const wava = () => {
+  return (
+    <div>
+    <header>
+      <h1 className="site-title">Creative Section Shapes</h1>
+    </header>
+    <section>
+      <div className='wrapper'>
+        <h2 className='section-title'></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    <section className='diagonal'>
+      <div className='wrapper'>
+        <h2 className='section-title'>Diagonal</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    <section className=''>
+      <div className='wrapper'>
+        <h2 className='section-title'>Some content here</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    <section className='spikes'>
+      <div className='wrapper'>
+        <h2 className='section-title'>Spikes</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    <section>
+      <div className='wrapper'>
+        <h2 className='section-title'>Another filler section</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    <section className='wavy'>
+      <div className='wrapper'>
+        <h2 className='section-title'>Wavy</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Ut aliquam augue sit amet malesuada finibus. Proin at consequat eros. In eu enim rutrum,
+             cursus ante bibendum, porttitor nisi. Etiam ullamcorper urna ut arcu ullamcorper tempor. Cras non porttitor metus.
+              Aliquam erat volutpat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default wava
